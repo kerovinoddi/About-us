8842717f4d89bdd82737fbf54b35a06f4421c3ac
user name <user email>
2020-05-29 12:39:07 +0300

    added p element

--- JQuery/script.js
+++ JQuery/script.js
@@ -1,33 +1,16 @@
 $(document).ready(function(event){
-$("#authenticate").submit(function(){
-    alert('submited')
-    event.preventDefault();
 
-})
-})
-$(document).ready(function(){
-    $("img").hover(function(){
-      $("img").fadeOut();
-    });
-    $("#button").click(function(){
-      $("img").show();
-    });
-  });
 
-$(document).ready(function(){
-    $("#button").hide(function(){
-        $("p").hide();
+
+    $(document).ready(function(event){
+        event.preventDefault();
+        $("#hide").click(function(){
+            $("p").hide();
+          });
+          
+          $("#show").click(function(){
+            $("p").show();
+          });
+          
+
     })
-    $("#button").show(function(){
-        $("p").show();
-        
-    })
-})
-$(document).ready(function(){
-    $("two").hover(function(){
-        $("#guys").hide();
-    })
-    $("#show").click(function(){
-        $("#guys").show();
-    });
-});
